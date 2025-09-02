88ce9204b68909fe59ef84314e11eb89db3f948b
ogzacky75 <ogzacky75@gmail.com>
2025-09-03 00:10:55 +0300

    adding class pet

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,20 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if not isinstance(name, str):
+            raise Exception("Pet name must be a string")
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Owner must be an Owner instance")
+
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
 
 class Owner:
     pass
